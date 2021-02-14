10d24a67ab4368ec36af877e3c21fedcba6dbc4c
Ozobywatel <ozobywatel@gmail.com>
2021-02-14 22:29:17 +0100

    first commit

---
+++ src/test/java/pl/coderslab/f/FaultManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.f;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaultManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
