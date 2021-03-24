a05db0eada9a5dee08911365beaba6ca9ab79a2f
Ozobywatel <ozobywatel@gmail.com>
2021-03-24 22:00:11 +0100

    404 attempt 0

--- src/main/java/pl/coderslab/controller/F404Controller.java
+++ src/main/java/pl/coderslab/controller/F404Controller.java
@@ -1,13 +1,7 @@
 package pl.coderslab.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import pl.coderslab.model.Project;
-import pl.coderslab.service.ProjectService;
-
-import java.util.List;
 
 @Controller
 
