28bd4639b1f9fd6d32acb29174d7373f5b4aafef
Ozobywatel <ozobywatel@gmail.com>
2021-03-31 19:38:35 +0200

    registration controller

--- src/main/java/pl/coderslab/controller/RegistrationController.java
+++ src/main/java/pl/coderslab/controller/RegistrationController.java
@@ -1,8 +1,38 @@
 package pl.coderslab.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import pl.coderslab.model.User;
 import pl.coderslab.service.UserService;
 
+import javax.validation.Valid;
+
+@Controller
 public class RegistrationController {
 
     private final UserService userService;
+
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public String showAddUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String saveUser(@Valid User user, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "users/register";
+        }
+        userService.add(user);
+        return "redirect:/login";
+    }
+
 }
